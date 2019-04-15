9d49dfd2aea016867d9fd910bc3c3c0c46a2aa39
frank870622 <frank870622@gmail.com>
2019-04-15 11:15:06 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,12 @@
+printf("hello\t", "sasaya\"ki", "\r\t\b\n\a\\\"\'");
+
+int a = -322;
+int b = +545;
+int c = - 654;
+int d = +65.65;
+int e = -55.1;
+
+printf("+");
+printf("printf");
+printf("\"\"\\n\t\r");
+printf("sdfsfsdsd\n");  
